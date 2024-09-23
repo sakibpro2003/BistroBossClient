cac58984b4e6b2cd3cbaafc6908ba28ebbff5dc9
Sakib Prodhan <sakibprodhan2003@gmail.com>
2024-09-24 00:56:44 +0600

    cart data shown in the navbar

--- src/Pages/Shared/FoodCard.jsx
+++ src/Pages/Shared/FoodCard.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import useAuth from "../../hooks/useAuth";
-import { axiosSecure } from "../../hooks/useAxiosSecure";
+import axiosSecure from "../../hooks/useAxiosSecure";
 const FoodCard = ({ item }) => {
   const { name, price, category, image, recipe, _id } = item;
   const { user } = useAuth();
--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "./useAxiosSecure";
 
 const useCart = () => {
-    
+  const axiosSecure = useAxiosSecure();
+  const { data: cart = [] } = useQuery({
+    queryKey: ["cart"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/cartData");
+      return res.data;
+    }
+  });
+  return [cart]
 };
 
 export default useCart;
