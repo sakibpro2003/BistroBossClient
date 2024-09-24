afc1e1f687787fc69e901d6415458c2756acf1f1
Sakib Prodhan <sakibprodhan2003@gmail.com>
2024-09-24 17:23:28 +0600

    cart data placed

--- src/Pages/Dashboard/Cart/Cart.jsx
+++ src/Pages/Dashboard/Cart/Cart.jsx
@@ -1,20 +1,61 @@
-import React from 'react';
-import useCart from '../../../hooks/useCart';
+import useCart from "../../../hooks/useCart";
+import { RiDeleteBinFill } from "react-icons/ri";
 
 const Cart = () => {
-    const [cart] = useCart();
-    const totalPrice = cart.reduce((total,item)=>total+item.price,0)
-    return (
-        <div className='flex justify-between'>
-            <h2 className="text-6xl">
-            Total item: {cart.length}
-
-            </h2>
-            <h2 className="text-6xl">
-                Total item: ${totalPrice}
-            </h2>
-        </div>
-    );
+  const [cart] = useCart();
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+  return (
+    <div>
+      <div className="flex justify-evenly">
+        <h2 className="text-6xl">Total item: {cart.length}</h2>
+        <h2 className="text-6xl">Total item: ${totalPrice}</h2>
+        <button className="btn bg-orange-400">Pay</button>
+      </div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map((item, index) => (
+              <tr key={item._id}>
+                <th>{index + 1}</th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img
+                          src={item.image}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                    <div></div>
+                  </div>
+                </td>
+                <td>
+                  <div className="font-bold">{item.name}</div>
+                </td>
+                <td>${item.price}</td>
+                <th>
+                  <button className="btn bg-orange-400 p-4 btn-xs text-2xl">
+                    <RiDeleteBinFill></RiDeleteBinFill>
+                  </button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
