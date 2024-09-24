9324a890d618854f2c07e1d4d2a014f7087f79bf
Sakib Prodhan <sakibprodhan2003@gmail.com>
2024-09-24 17:01:43 +0600

    total price using reduce method

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,19 +1,50 @@
-import { NavLink, Outlet } from 'react-router-dom';
-import { FaShoppingCart } from "react-icons/fa";
+import { NavLink, Outlet } from "react-router-dom";
+import { FaAd, FaCalendar, FaHome, FaShoppingCart } from "react-icons/fa";
 
 const Dashboard = () => {
-    return (
-        <div className='flex'>
-            <div className='w-64 min-h-full bg-orange-400'>
-                <ul className='menu'>
-                    <li><NavLink to={'dashboard/cart'}> <FaShoppingCart /> My Cart</NavLink></li>
-                </ul>
-            </div>
-            <div className="flex-1">
-                <Outlet></Outlet>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex">
+      <div className="w-64 min-h-screen bg-orange-400">
+        <ul className="menu">
+          <li>
+            <NavLink to={"dashboard/cart"}>
+              <FaShoppingCart /> My Cart
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"dashboard/userHome"}>
+              <FaHome /> User Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"dashboard/reservation"}>
+              <FaCalendar /> Reservation
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"dashboard/review"}>
+              <FaAd /> Add a Review
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"dashboard/bookings"}>
+              <FaAd /> Add a Bookings
+            </NavLink>
+          </li>
+          <div className="divider"></div>
+
+          <li>
+            <NavLink to={"dashboard/home"}>
+              <FaHome /> Home
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="flex-1">
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
--- src/Pages/Dashboard/Cart/Cart.jsx
+++ src/Pages/Dashboard/Cart/Cart.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import useCart from '../../../hooks/useCart';
 
 const Cart = () => {
+    const [cart] = useCart();
+    const totalPrice = cart.reduce((total,item)=>total+item.price,0)
     return (
-        <div>
+        <div className='flex justify-between'>
             <h2 className="text-6xl">
-                My Cart
+            Total item: {cart.length}
+
+            </h2>
+            <h2 className="text-6xl">
+                Total item: ${totalPrice}
             </h2>
         </div>
     );
