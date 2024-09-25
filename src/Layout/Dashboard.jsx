4a78030c5a6109776758efde34c1212e706591c4
Sakib Prodhan <sakibprodhan2003@gmail.com>
2024-09-25 19:17:04 +0600

    duplicate user detail filtered in databse

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,14 +1,18 @@
 import { NavLink, Outlet } from "react-router-dom";
-import { FaAd, FaCalendar, FaHome, FaShoppingCart } from "react-icons/fa";
+import { FaAd, FaCalendar, FaEnvelope, FaHome, FaList, FaShoppingCart } from "react-icons/fa";
 import useCart from "../hooks/useCart";
 
 const Dashboard = () => {
+
+  const isAdmin = true;
     const [cart] = useCart();
   return (
     <div className="flex">
       <div className="w-64 min-h-screen bg-orange-400">
         <ul className="menu">
-          <li>
+         {
+            isAdmin ? <>
+             <li>
             <NavLink to={"dashboard/cart"}>
               <FaShoppingCart /> My Cart ({cart.length})
             </NavLink>
@@ -33,11 +37,52 @@ const Dashboard = () => {
               <FaAd /> Add a Bookings
             </NavLink>
           </li>
+            </>:<>
+             <li>
+            <NavLink to={"dashboard/cart"}>
+              <FaShoppingCart /> My Cart ({cart.length})
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"dashboard/userHome"}>
+              <FaHome /> User Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"dashboard/reservation"}>
+              <FaCalendar /> Reservation
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"dashboard/review"}>
+              <FaAd /> Add a Review
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"dashboard/bookings"}>
+              <FaAd /> Add a Bookings
+            </NavLink>
+          </li>
+            </>
+         }
+
+
+          {/* normal user sidebaer */}
           <div className="divider"></div>
 
           <li>
             <NavLink to={"dashboard/home"}>
               <FaHome /> Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"dashboard/home"}>
+              <FaList /> Menu
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"dashboard/home"}>
+              <FaEnvelope /> Contact
             </NavLink>
           </li>
         </ul>
