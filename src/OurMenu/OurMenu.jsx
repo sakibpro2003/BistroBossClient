2ddc1402cf612714dc7b4b097fa000458459e76b
Sakib Prodhan <sakibprodhan2003@gmail.com>
2024-10-31 13:09:10 +0600

    all categories added to OurMenu

--- src/OurMenu/OurMenu.jsx
+++ src/OurMenu/OurMenu.jsx
@@ -1,10 +1,29 @@
 import OurMenuBanner from "../assets/Components/OurMenuBanner";
 import banner3 from '../../src/assets/menu/banner3.jpg';
+import useMenu from "../hooks/useMenu";
+import ShortMenu from "../assets/Components/ShortMenu";
+
 
 const OurMenu = () => {
+    const dessertBg = 'https://i.ibb.co.com/wNFxCZG/pexels-igor-ovsyannykov-56123-205961.jpg';
+    const pizzaBg = 'https://i.ibb.co.com/XpxfJ3t/pexels-narda-yescas-724842-1566837.jpg';
+    const saladBg = 'https://i.ibb.co.com/PrLKVpY/pexels-jerchung-2116090.jpg';
+    const soupBg = 'https://i.ibb.co.com/Lz217NW/pexels-navada-ra-628779-1703272.jpg';
+    const [menu] = useMenu();
+    console.log(menu)
     return (
         <div>
             <OurMenuBanner bgImage={banner3}  title="OUR MENU" sub_title="WOULD YOU LIKE TO TRY OUR DISH?"/>
+            {/* TODO: modify short menu after uploading new menu images */}
+            <ShortMenu></ShortMenu>
+            <OurMenuBanner bgImage={dessertBg} title={'DESSERTS'} sub_title={" TRysidfis sd fjklsdj fsk dfk sdfj skdjf"}></OurMenuBanner>
+            <ShortMenu></ShortMenu>
+            <OurMenuBanner bgImage={pizzaBg} title={'PIZZA'} sub_title={" TRysidfis sd fjklsdj fsk dfk sdfj skdjf"}></OurMenuBanner>
+            <ShortMenu></ShortMenu>
+            <OurMenuBanner bgImage={saladBg} title={'SALAD'} sub_title={" TRysidfis sddsklfj skdjf fjklsdj fsk dfk sdfj skdjf"}></OurMenuBanner>
+            <ShortMenu></ShortMenu>
+            <OurMenuBanner bgImage={soupBg} title={'SOUP'} sub_title={" TRysi s sddsklfj skdjf fjklsdj fsk dfk sdfj skdjf"}></OurMenuBanner>
+            <ShortMenu></ShortMenu>
         </div>
     );
 };
---
+++ src/assets/Components/ShortMenu.jsx
@@ -0,0 +1,31 @@
+// import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+// import useMenu from '../hooks/useMenu';
+import useMenu from "../../hooks/useMenu";
+import MenuItem from "../../../src/MenuItem/MenuItem";
+// import useMenu from "";
+
+const ShortMenu = () => {
+  const [menu] = useMenu();
+  const popular = menu.filter((item) => item.category === "popular");
+
+  return (
+    <section className="mb-12">
+      {/* <SectionTitle
+                heading="From Our Menu"
+                subHeading="Popular Items"
+            ></SectionTitle> */}
+      <div className="grid md:grid-cols-2 gap-10">
+        {popular.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+      <div className="flex justify-center">
+        <button className="btn  btn-outline border-0 border-b-4 mt-4">
+          View Full Menu
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default ShortMenu;
